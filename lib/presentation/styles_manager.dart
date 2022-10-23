33f7c18c7565d11b16d32cb65e70063a1c1f9a99
JAbozarOid <araghibdoust@hotmail.com>
2022-10-23 12:58:05 +0330

    styles manager

---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,47 @@
+import 'package:cleanarch/presentation/font_manager.dart';
+import 'package:flutter/material.dart';
+
+// create a private function
+TextStyle _getTextStyle(
+    double fontSize, String fontFamily, FontWeight fontWeight, Color color) {
+  return TextStyle(
+      fontSize: fontSize,
+      fontFamily: fontFamily,
+      fontWeight: fontWeight,
+      color: color);
+}
+
+// regular text style
+// optional parameters goes inside of {}
+TextStyle getRegularStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.regular, color);
+}
+
+// light text style
+TextStyle getLightStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.light, color);
+}
+
+// bold text style
+TextStyle getBoldStyle({double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.bold, color);
+}
+
+// semi bold text style
+TextStyle getSemiBoldStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.semiBold, color);
+}
+
+// medium bold text style
+TextStyle getMediumStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.medium, color);
+}
