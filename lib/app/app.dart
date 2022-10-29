9558a0dd62b2531ecf6a59aebcc64f331b9c19dd
JAbozarOid <araghibdoust@hotmail.com>
2022-10-29 18:05:07 +0330

    timer_splash_view

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:cleanarch/presentation/resources/routes_manager.dart';
 import 'package:flutter/material.dart';
 
 import '../presentation/resources/theme_manager.dart';
@@ -22,6 +23,10 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: getApplicationTheme(),);
+    return MaterialApp(
+        onGenerateRoute: RouteGenerator.getRoute,
+        initialRoute: Routes.splashRoute,
+        debugShowCheckedModeBanner: false,
+        theme: getApplicationTheme());
   }
 }
