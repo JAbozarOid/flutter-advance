1aee994f9b7dc81cc9a15b133e43a47e326abe35
JAbozarOid <araghibdoust@hotmail.com>
2022-10-30 00:38:03 +0330

    onBoarding structure

--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -11,4 +11,7 @@ class AppStrings {
   static const String onBoardingSubTitle2 = "Tut app is an awesome flutter application using clean architecture #2";
   static const String onBoardingSubTitle3 = "Tut app is an awesome flutter application using clean architecture #3";
   static const String onBoardingSubTitle4 = "Tut app is an awesome flutter application using clean architecture #4";
+
+  // onBoarding skip button text
+  static const String skip = "Skip";
 }
