3c6b2e88a043a07dac714f79fc45263748db90c1
JAbozarOid <araghibdoust@hotmail.com>
2022-11-05 16:14:30 +0330

    validate email unit test

---
+++ test/validator_email_test.dart
@@ -0,0 +1,28 @@
+import 'package:cleanarch/utils/validator_email.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('validate for empty email id', () {
+    // Arrange and Act
+    var result = ValidatorEmail.validateEmail('');
+
+    // assert
+    expect(result, "Required Filed");
+  });
+
+  test('validate for invalid email id', () {
+    // Arrange and Act
+    var result = ValidatorEmail.validateEmail('sadsdasdsad');
+
+    // assert
+    expect(result, "Please Enter a valid email id");
+  });
+
+  test('validate for valid email id', () {
+    // Arrange and Act
+    var result = ValidatorEmail.validateEmail('ab@gmail.com');
+
+    // assert
+    expect(result, null);
+  });
+}
