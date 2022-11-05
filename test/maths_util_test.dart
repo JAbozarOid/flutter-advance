b900931827e7bdcee6cc0a7014075071c59e397d
JAbozarOid <araghibdoust@hotmail.com>
2022-11-05 15:13:59 +0330

    group unit test

--- test/maths_util_test.dart
+++ test/maths_util_test.dart
@@ -2,16 +2,42 @@ import 'package:cleanarch/utils/maths_util.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('check for two number addition', () {
+  // all test function in a one class can goes in group
+  group('Maths utils -', () {
+    test('check for two number addition', () {
+      // Arrange
+      var a = 10;
+      var b = 10;
 
-    // Arrange
-    var a = 10;
-    var b = 10;
+      // Act
+      var sum = add(a, b);
 
-    // Act
-    var sum = add(a, b);
+      // Assert
+      expect(sum, 20);
+    });
 
-    // Assert
-    expect(sum, 20);
+    test('check for two number substraction', () {
+      // Arrange
+      var a = 10;
+      var b = 10;
+
+      // Act
+      var subs = substract(a, b);
+
+      // Assert
+      expect(subs, 0);
+    });
+
+    test('check for two number multiply', () {
+      // Arrange
+      var a = 10;
+      var b = 10;
+
+      // Act
+      var mul = multiply(a, b);
+
+      // Assert
+      expect(mul, 100);
+    });
   });
 }
